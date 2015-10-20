466b7724535f6b4ff169197da20e1abffeccc3af
Rashid <rashidyusof196@gmail.com>
2015-10-20 15:54:06 +0100

    proj files

---
+++ Escape from Senghetto/finalItems.py
@@ -0,0 +1,172 @@
+item_pen = 	{"id" : "pen" ,
+			"name": "a pen",
+			"description": "A small green ballpoint pen. Who buys green pens?",
+			"damage_beggar": 20, 
+			"damage_player": 5,
+                 "usage" : False,
+                 "room_to_use" : "",
+                 "item_effect" : ""
+			}
+
+item_plastic_knife = 	{"id" : "knife" , 
+						"name" : "a plastic knife", 
+						"description": "A leftover plastic knife from a late night cheesy chip.",
+						"damage_beggar": 0, 
+						"damage_player": 10,
+                                  "usage" : False,
+                                  "room_to_use" : "",
+                                  "item_effect" : ""
+						}
+
+item_toilet_roll = 	{"id" : "roll" , 
+					"name" : "some toilet roll", 
+					"description": "Some toilet roll. Tesco own brand. It's like sandpaper.",
+					"damage_beggar": 0, 
+					"damage_player": 10,
+                             "usage" : False,
+                             "room_to_use" : "",
+                             "item_effect" : ""
+					}
+
+item_frying_pan = 	{"id" : "pan", 
+					"name" : "a frying pan", 
+					"description": "A non-stick frying pan with bits of burnt bacon sticking to it.",
+					"damage_beggar": 40, 
+					"damage_player": 5,
+                             "usage" : False,
+                             "room_to_use" : "",
+                             "item_effect" : ""
+					}
+
+item_bulb = 	{"id" : "bulb" , 
+				"name" : "a bulb", 
+				"description": "It is a bright idea to carry a bulb with you at all times... Bright idea? C'mon guys, tough crowd?",
+				"damage_beggar": 30, 
+				"damage_player": 30,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_sponge = 	{ "id" : "sponge" , 
+				"name" : "a sponge", 
+				"description": "A sponge. Very absorbant. Doesn't look like it has been used much.",
+				"damage_beggar": 0, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_belt = 	{ "id" : "belt" , 
+				"name" : "a belt", 
+				"description": "Standard belt for holding up a pair of trousers. Can also fit around ones head as a hilarious joke when drunk.",
+				"damage_beggar": 5, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_stone = 	{ "id" : "stone" , 
+				"name" : "a stone", 
+				"description": "Wow man, that's totally a stone dude.",
+				"damage_beggar": 30, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_pumpkin = 	{ "id" : "pumpkin" , 
+				"name" : "a pumpkin",
+				"description": "A pumpkin, there seems to be some sort of phallic symbol carved into the side.", 
+				"damage_beggar": 5, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_phone = 	{ "id" : "phone" , 
+				"name" : "your cell phone", 
+				"description": "Your trusty mobile phone. You never go anywhere without it. It's dead, as usual.",
+				"damage_beggar": 10, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_wand = 	{ "id" : "wand" , 
+				"name" : "a wand", 
+				"description": "A magic wand, who knows what mysterious items and spells you can conjour with this. Wizard hat sold seperately.",
+				"damage_beggar": 80, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_bacon = 	{ "id" : "bacon" , 
+				"name" : "some bacon", 
+				"description": "Delicious bacon, 64'%'' pork, 100'%' amazing. I bet this smells delicious when being cooked.",
+				"damage_beggar": 0, 
+				"damage_player": 5,
+                       "usage" : False,
+                       "room_to_use" : "Kitchen",
+                       "item_effect" : "You cook the bacon in a pan. It smells delicious. Suddenly, you hear the sound of wet footsteps from the bathroom. Ahh yes, the power of bacon." 
+				}
+
+item_laptop = 	{ "id" : "laptop" , 
+				"name" : "your laptop", 
+				"description": "Your laptop is... well it has Microsoft Word and Netflix, and that's good enough for you.",
+				"damage_beggar": 50, 
+				"damage_player": 10,
+                       "usage" : False,
+                       "room_to_use" : "Kitchen",
+                       "item_effect" : "You sell your laptop to the guy for £100 and now have enough money for the wand. You're not sure how you're going to do your assignments now. But hey wands right? totally worth it. (What are you doing with your life?)"
+				}
+
+item_shoes = 	{ "id" : "shoes" , 
+				"name" : "beggar's shoes", 
+				"description": "Apart from the scent of urine, these shoes are quite comfy.",
+				"damage_beggar": 0, 
+				"damage_player": 0,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_boxers = 	{ "id" : "boxers" , 
+				"name" : "your boxers", 
+				"description": "Calvin Klein boxers. Who are you kidding, you imported them from China for £2.50 a pair.",
+				"damage_beggar": 0, 
+				"damage_player": 0,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_top = 		{ "id" : "top" , 
+				"name" : "your t-shirt", 
+				"description": "Primarks finest. I hope those yellow stains aren't what they smell like...",
+				"damage_beggar": 0, 
+				"damage_player": 0,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+
+item_trousers = { "id" : "trousers" , 
+				"name" : "your trousers", 
+				"description": "The great thing about jeans is you can wear them even when you rip them. ",
+				"damage_beggar": 0, 
+				"damage_player": 0,
+                       "usage" : False,
+                       "room_to_use" : "",
+                       "item_effect" : ""
+				}
+    
+clothes = [item_shoes, item_boxers, item_top, item_trousers]
+
---
+++ Escape from Senghetto/player.py
@@ -0,0 +1,10 @@
+from items import *
+from map import rooms
+
+inventory = [item_bacon]
+
+wearing = []
+
+    
+# Start game at the reception
+current_room = rooms["Bedroom"]
